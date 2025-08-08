567570ddb17cc794155c4d7017872d1eb2242f4c
Ludger Solbach <soulman@soulspace.org>
2025-08-08 16:22:30 +0200

    updated qclojure dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.soulspace/qclojure-ml "0.1.0-SNAPSHOT"
   :description "A Clojure Backend for Amazon Braket"
-  :dependencies [[org.soulspace/qclojure "0.9.0"]
+  :dependencies [[org.soulspace/qclojure "0.10.0"]
                  [org.scicloj/noj "2-beta18"]
                  [org.scicloj/clay "2-beta45"]])
