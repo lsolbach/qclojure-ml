6ec436d5a0c567a6bbcbc976509a2200e5ac4488
Ludger Solbach <soulman@soulspace.org>
2025-08-28 18:07:18 +0200

    updated project definition

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
 (defproject org.soulspace/qclojure-ml "0.1.0-SNAPSHOT"
-  :description "A Clojure Backend for Amazon Braket"
-  :dependencies [[org.soulspace/qclojure "0.10.0"]
+  :description "Quantum Machine Learning algorithms for QClojure"
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.soulspace/qclojure "0.12.0"]
                  [org.scicloj/noj "2-beta18"]
-                 [org.scicloj/clay "2-beta45"]])
+                 [org.scicloj/clay "2-beta52"]]
+
+  :scm {:name "git" :url "https://github.com/lsolbach/qclojure-ml"}
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]])
+
