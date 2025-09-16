5dd59476397a4c97bafa27d9421107141cc570bf
Ludger Solbach <soulman@soulspace.org>
2025-09-16 08:27:51 +0200

    added initial namespaces for qgan and quantum kernel methods

---
+++ src/org/soulspace/qclojure/application/ml/qgan.clj
@@ -0,0 +1,4 @@
+(ns org.soulspace.qclojure.application.ml.qgan
+  "Common infrastructure for quantum generative adversarial networks (QGANs)"
+  (:require [org.soulspace.qclojure.application.backend :as backend]
+            ))
---
+++ src/org/soulspace/qclojure/application/ml/quantum_kernel.clj
@@ -0,0 +1,4 @@
+(ns org.soulspace.qclojure.application.ml.quantum-kernel
+  "Common infrastructure for quantum kernel methods"
+  (:require [org.soulspace.qclojure.application.backend :as backend]
+            ))
