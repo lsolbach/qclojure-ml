c6c6189c0acc37190ae359344397426a9c3942b1
Ludger Solbach <soulman@soulspace.org>
2025-10-11 09:54:02 +0200

    update qclojure to 0.20.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.3"]
-                 [org.soulspace/qclojure "0.19.0"]
+                 [org.soulspace/qclojure "0.20.0"]
                  [org.scicloj/noj "2-beta18"]]
 
   :profiles {:dev [:user {}]
