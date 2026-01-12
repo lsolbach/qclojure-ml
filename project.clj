f113ea3fef3c3b27714e7c74bd342e60d19e4fe5
Ludger Solbach <soulman@soulspace.org>
2026-01-12 03:15:45 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Quantum Machine Learning algorithms for QClojure"
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.3"]
-                 [org.scicloj/noj "2-beta18"]
+  :dependencies [[org.clojure/clojure "1.12.4"]
+                 [org.scicloj/noj "2-beta19.1"]
                  [org.slf4j/slf4j-nop "2.0.17"] ; supress logging warnings
-                 [org.soulspace/qclojure "0.23.0"]]
+                 [org.soulspace/qclojure "0.24.0"]]
 
   :profiles {:dev [:user {}]
              :sim-heavy {:jvm-opts ["-Xms8g" "-Xmx32g"
@@ -13,7 +13,7 @@
                                     "-XX:+AlwaysPreTouch"]}
              :container {:jvm-opts ["-XX:InitialRAMPercentage=2.0"
                                     "-XX:MaxRAMPercentage=60.0"]}
-             :clay {:dependencies [[org.scicloj/clay "2.0.0"]]
+             :clay {:dependencies [[org.scicloj/clay "2.0.5"]]
                     :source-paths ["src" "notebooks"]}}
 
   :scm {:name "git" :url "https://github.com/lsolbach/qclojure-ml"}
